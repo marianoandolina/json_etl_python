e089ba686559d004a623dacfb997558ac559af69
Mariano Andolina <mariano.3gs@gmail.com>
2023-01-04 14:00:03 -0300

    1st commit

--- ejemplos_clase/ejemplo_2.py
+++ ejemplos_clase/ejemplo_2.py
@@ -15,18 +15,28 @@ __email__ = "alumnos@inove.com.ar"
 __version__ = "2.0"
 
 import json
-import requests
+import requests  # El metodo request permite hacer una peticion de informacion a una url
 
 
 if __name__ == '__main__':
-    print("Bienvenidos a otra clase de Inove con Python")
-    #
+    print("Bienvenidos a otra clase de Inove con Python")    
     print('# ---- Ejemplos con JSON Request ---- #')
 
-    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1") # Solicitamos la info a la url
+    
     # Se puede obtener el objeto JSON de dos formas distintas
-    data = json.loads(response.text)
+    # Forma nro 1
+    '''De esta manera tenemos que buscar en que parte de la url esta la info en json y pasarla como parametro (respnse.text), en este caso en text esta la info
+     en json de la url'''
+
+    data = json.loads(response.text) 
+    
+    # Forma nro2 
+    ''' La siguiente forma es la correcta porque es mas simple, con el metodo response.json() se encarga de buscar donde esta la info en formato json y guardarla
+    en la variable'''
+    
     data = response.json()
+
     print('Imprimir los datos traídos de la nube')
     print(json.dumps(data, indent=4))
 
